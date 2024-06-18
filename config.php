07c98f30c2093d7ca1091d2c7bc8fd44412297ce
Douglas Bastos <douglasmoreirabastos@gmail.com>
2024-06-17 23:36:10 -0300

    alteração em partes do código para conectar ao BD

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
 
-    $dbHost = 'sql200.infinityfree.com';
-    $dbUsername = 'if0_36329043';
-    $dbPassword = 'h0QqWd44FV';
-    $dbName = 'if0_36329043_sislogin';
+    $dbHost = 'sql311.infinityfree.com';
+    $dbUsername = 'if0_36069799';
+    $dbPassword = 'UvogPnplqUN';
+    $dbName = 'if0_36069799_douglastatoo';
     
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
-     if($conexao->connect_errno)
-     {
-         echo "Erro";
-     }
-     else
-     {
-         echo "Conexão efetuada com sucesso";
-     }
+    // if($conexao->connect_errno)
+    // {
+    //     echo "Erro";
+    // }
+    // else
+    // {
+    //     echo "Conexão efetuada com sucesso";
+    // }
 
 ?>
